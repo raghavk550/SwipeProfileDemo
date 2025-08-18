eee8efce0aedb41a625d82d6768edbe2419242c0
Raghav Kakria <raghav.kakria@appinventiv.com>
2025-08-18 16:11:09 +0530

    Initial Commit

---
+++ ProfileScreenDemo/ProfileScreenDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileScreenDemoApp.swift
+//  ProfileScreenDemo
+//
+//  Created by Raghav Kakria on 18/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ProfileScreenDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
